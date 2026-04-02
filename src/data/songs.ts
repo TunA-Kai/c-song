be682645c60f769842642c83e6332a7e90f6f8ae
Kai Nguyen <emailtolearncode@gmail.com>
2026-04-02 21:01:04 +0700

    feat: add new song "所有的美好终会属于你" with lyrics and annotations

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -1,9 +1,11 @@
 import songBieQuanWoChiKu from "../../songs/别劝我吃苦.md?raw";
 import songYuanMeiHao from "../../songs/愿所有美好都会奔向你.md?raw";
+import song3 from "../../songs/所有的美好终会属于你.md?raw";
 import { parseSongMarkdown } from "../lib/parseSongMarkdown";
 import type { Song } from "../models/song";
 
 export const songs: Song[] = [
   parseSongMarkdown(songBieQuanWoChiKu, "别劝我吃苦"),
   parseSongMarkdown(songYuanMeiHao, "愿所有美好都会奔向你"),
+  parseSongMarkdown(song3, "所有的美好终会属于你"),
 ];
