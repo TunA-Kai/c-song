f018b9390dee95c7e417e9f78abb9872d4eb6a65
Kai Nguyen <emailtolearncode@gmail.com>
2026-06-02 21:06:08 +0700

    Add lyrics and annotations for "你不必是一朵花" and "生活没有说明书"

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -3,6 +3,9 @@ import songYuanMeiHao from "../../songs/愿所有美好都会奔向你.md?raw";
 import song3 from "../../songs/所有的美好终会属于你.md?raw";
 import song4 from "../../songs/背包很小要装满快乐.md?raw";
 import songShiMaMaShiNvEr from "../../songs/是妈妈是女儿.md?raw";
+import songNiBuBiShiYiDuoHua from "../../songs/你不必是一朵花.md?raw";
+import songShengHuoMeiYouShuoMingShu from "../../songs/生活没有说明书.md?raw";
+import songRenShengYouDuoShaoJinTianDuoShaoMingTian from "../../songs/人生有多少今天多少明天.md?raw";
 import { parseSongMarkdown } from "../lib/parseSongMarkdown";
 import type { Song } from "../models/song";
 
@@ -12,4 +15,10 @@ export const songs: Song[] = [
   parseSongMarkdown(song3, "所有的美好终会属于你"),
   parseSongMarkdown(song4, "背包很小要装满快乐"),
   parseSongMarkdown(songShiMaMaShiNvEr, "是妈妈是女儿"),
+  parseSongMarkdown(songNiBuBiShiYiDuoHua, "你不必是一朵花"),
+  parseSongMarkdown(songShengHuoMeiYouShuoMingShu, "生活没有说明书"),
+  parseSongMarkdown(
+    songRenShengYouDuoShaoJinTianDuoShaoMingTian,
+    "人生有多少今天多少明天",
+  ),
 ];
